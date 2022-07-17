a1852c5033e11c2babc881b5e3f63b77903d6fdd
buzzcola3 <buzzcola3@gmail.com>
2022-07-17 09:08:12 +0200

    cleanup

--- esp_webserver_code/WidgetsOnGrid.js
+++ esp_webserver_code/WidgetsOnGrid.js
@@ -10,43 +10,41 @@ export class WidgetsOnGrid{
         this.gridSegmentTopGap = gridSegmentTopGap;
         this.gridSegmentLeftGap = gridSegmentLeftGap;
         this.targetDivID = targetDivID;
-        this.sizeUnit = sizeUnit;
+        this.sizeUnit = sizeUnit;        
 
-        this.create = this.create;
+
+        this.pushNewInstanceToList();
 
         let gridID = targetDivID + '_grid';
-
-
-        WidgetsOnGrid.grid.instances.push({
-            treeStructAddr: undefined,
-            targetDivID: targetDivID,
-            gridID: gridID,
-            gridSegmentWidth: gridSegmentWidth,
-            gridSegmentHeight: gridSegmentHeight,
-            gridSegmentTopGap: gridSegmentTopGap,
-            gridSegmentLeftGap: gridSegmentLeftGap,
-            sizeUnit: sizeUnit,
-            gridWidth: undefined,
-            gridHeight: undefined,
-            lastKnownPointerPosition: {X: 'unchecked', Y: 'unchecked'},
-        });
-
-
         let instance = WidgetsOnGrid.assignInstanceNumber(targetDivID)
+
         let gridInstance = WidgetsOnGrid.grid.instances[instance];
         this.gridInstance = gridInstance;
 
 
-        let Tree = new WidgetStructure(targetDivID, gridID, ManageGrid.main.getGridEl(gridInstance));
+        //create the grid
+        new WidgetStructure(targetDivID, gridID, ManageGrid.main.getGridEl(gridInstance));
         let gridParentObjAddr = WidgetStructure.getLastest();
         gridInstance.treeStructAddr = gridParentObjAddr;
-
         window.onresize = WidgetsOnGrid.resizeHandler;
-        WidgetsOnGrid.displayGridOnScreen(gridParentObjAddr);
 
+        WidgetsOnGrid.displayGridOnScreen(gridParentObjAddr);
     }
 
     static grid = {instances: [], widgetSheet: {data: [], busy: false}};
+    pushNewInstanceToList(){
+        WidgetsOnGrid.grid.instances.push({
+            treeStructAddr: undefined,
+            targetDivID: this.targetDivID,
+            gridSegmentWidth: this.gridSegmentWidth,
+            gridSegmentHeight: this.gridSegmentHeight,
+            gridSegmentTopGap: this.gridSegmentTopGap,
+            gridSegmentLeftGap: this.gridSegmentLeftGap,
+            sizeUnit: this.sizeUnit,
+            gridWidth: undefined,
+            gridHeight: undefined,
+        });
+    }
 
     static assignInstanceNumber(targetDivID){
         let i = 0;
@@ -59,12 +57,11 @@ export class WidgetsOnGrid{
     }
 
     static displayGridOnScreen(gridParentObjAddr){
-        console.log(WidgetStructure.getAddressObj(gridParentObjAddr).display)
-        console.log(WidgetStructure.getAddressObj(gridParentObjAddr));
+        WidgetStructure.getAddressObj(gridParentObjAddr).display();  
+    }
 
-        WidgetStructure.getAddressObj(gridParentObjAddr).display();
-
-        
+    static hideGridOnScreen(gridParentObjAddr){
+        WidgetStructure.getAddressObj(gridParentObjAddr).hide();  
     }
 
     static resizeHandler(){ //mainGrid
@@ -76,5 +73,42 @@ export class WidgetsOnGrid{
         }
     }
 
+    create(widget, height, width, posX, posY){
+        //get position code
+        //get widget code
+        //combine those
+        //slap it in widgetStructure
+
+
+    }
+
+
+    static async fetchWidgetDataFromJson(widgetName, widgetID, instanceNum, retry = 0){
+        if(WidgetsOnGrid.grid.widgetSheet.busy == true){
+            retry++;
+            if(retry ==  20){console.error('failed To  the Widget'); return;}
+            setTimeout( function(){WidgetsOnGrid.fetchWidgetDataFromJson(widgetName, instanceNum, retry)}, 100);
+            return;
+        };
+        console.log('fetching: ' + widgetName)
+        WidgetsOnGrid.grid.widgetSheet.busy = true;
+
+        let data = await fetch('./widgets.json')
+        let out = await data.json();
+        {
+            if(await out[widgetName] === undefined){console.error("widget not found")}
+            else{
+                WidgetsOnGrid.grid.widgetSheet.busy = false;
+
+                let gridInstance = WidgetsOnGrid.grid.instances[instanceNum];
+                let testCode = new parseWidgetCode(out[widgetName], widgetID, gridInstance);
+                testCode = testCode.get();
+                WidgetsOnGrid.grid.widgetSheet.data.push(testCode);
+                
+
+                return;
+            }
+        }
+    }
 }
 window.WidgetsOnGrid = WidgetsOnGrid;
